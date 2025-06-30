52a6b4ee479b37584a12f885976370d91b332646
VISHAL RAJ <121842228+Vishalraj134@users.noreply.github.com>
2025-06-30 22:48:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import process from "process";
 const apiKey = process.env.GEMINI_API_KEY || "AIzaSyBoNi4cnjN6Gf8JhOmPpegd2a_25xQckiE";
 const genAI = new GoogleGenerativeAI(apiKey);
 
-app.use(cors({origin:["https://story-genertor.vercel.app/"]}));
+app.use(cors({origin:["https://localhost:3000","https://story-genertor.onrender.com"]}));
 app.use(express.json());
 
 app.post("/generate-story", async (req, res) => {
